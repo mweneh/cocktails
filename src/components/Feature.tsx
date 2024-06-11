bf2bff973269ab03584bcc225c91d578b410437a
David <davidongaroh@gmail.com>
2024-06-11 21:55:16 +0300

    more images for featured section

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Dash from './Dash'
+import Image from 'next/image'
 
 function Feature() {
   return (
@@ -13,6 +15,68 @@ function Feature() {
         repellat expedita tempore hic iste. Laborum odit illo ex nostrum! Lorem
         ipsum dolor sit amet.
       </p>
+      < Dash />
+
+      <div className='grid md:grid-cols-[1fr,37%,1fr] gap-16 mt-10 '>
+        <div className='w-fit mx-auto self-end '>
+          <Image 
+          className= "w-[100%] max-w-[400px] sm:max-w-full h-auto shadow-2xl rounded-3xl"
+          src='/hero3.jpg'
+          width={300}
+          height={600}
+          alt="grid image" 
+          />
+
+          <div className="space-y-4">
+            <Dash />
+            <h2 className="font-medium text-xl">Memento Strong</h2>
+            <p className="text-gray-700 text-[14px] xl:text-[16px]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos, nesciunt distinctio.
+            </p>
+          </div>
+        </div>
+
+
+        <div className='w-fit mx-auto '>
+          <Image 
+          className= "w-[100%] max-w-[400px] sm:max-w-full h-auto shadow-2xl rounded-3xl"
+          src='/tail3.jpg'
+          width={500}
+          height={900}
+          alt="grid image" 
+          />
+
+          <div className="space-y-4">
+            <Dash />
+            <h2 className="font-medium text-xl">Long Island</h2>
+            <p className="text-gray-700 text-[14px] xl:text-[16px]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos, nesciunt distinctio.
+            </p>
+          </div>
+        </div>
+
+
+        <div className='w-fit mx-auto self-end '>
+          <Image 
+          className= "w-[100%] max-w-[400px] sm:max-w-full h-auto shadow-2xl rounded-3xl"
+          src='/tail2.jpg'
+          width={300}
+          height={600}
+          alt="grid image" 
+          />
+
+          <div className="space-y-4">
+            <Dash />
+            <h2 className="font-medium text-xl">Jamaican Girl</h2>
+            <p className="text-gray-700 text-[14px] xl:text-[16px]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos, nesciunt distinctio.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ function Hero() {
   return (
     <div className='relative min-h-screen'>
         <Image 
-        className='lg:w-[580px] xl:w-[620px] h-auto absolute right-0 top-0 -z-10'
+        className='lg:w-[580px] xl:w-[620px] h-auto absolute right-0 top-0 -z-10 rounded-2xl'
         src="/hero2.jpg" 
         width={1000} 
         height={600} 
