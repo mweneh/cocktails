e47e7cfb6199fd16fdb1d92f346ae176414b28ce
David <davidongaroh@gmail.com>
2024-06-11 14:55:17 +0300

    Dash compnent done

---
+++ src/components/Dash.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Dash  () {
+  return (
+    <div className='bg-accent h-[5px] w-[70px] mt-10 ' /> 
+  )
+}
+
+export default Dash  
---
+++ src/components/Feature.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Feature() {
+  return (
+    <div className='container pt-40'>
+      <h2 className="text-6xl font-bold">Our</h2>
+      <h2 className="text-6xl font-bold pt-2">
+        Featured <span className="text-accent">Drinks</span>
+      </h2>
+
+      <p className="max-w-[550px] pt-10 text-gray-700">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa placeat
+        repellat expedita tempore hic iste. Laborum odit illo ex nostrum! Lorem
+        ipsum dolor sit amet.
+      </p>
+    </div>
+  )
+}
+
+export default Feature
