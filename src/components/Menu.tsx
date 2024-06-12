1cb86fafa97f13397b3db9731bb9097bc7f355a9
David <davidongaroh@gmail.com>
2024-06-12 16:26:33 +0300

    more detailed desc

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -8,30 +8,30 @@ const menuData = [
    title:"110 in the shade",
    img:"/shade.jpg",
    price:"$10.5",
-   desc:"Description"
+   desc:"A James Bond favorite, the Dry Martini is simple and elegant. While we don't know the exact origin of the Dry Martini, we do know that the cocktail made with dry gin, dry vermouth, and orange bitters is best served with a lemon twist."
   },
   {
     img: "/magarita.jpg",
     title: "Magarita",
-    desc: "Tequila,Triple sec,Lime , Contemporary Classics, Alcoholic,Cocktail glass",
+    desc: "The Margarita originated in Mexico, and it sparks a lot of debate about the way it's served. Whether you like it with or without a salt rim, frozen or on the rocks, or with triple sec, Cointreau, or Grand Marnier, you can't go wrong with a classic Margarita. The tequila is key, so opt for blanco tequila.",
     price: "$9.50",
   },
   {
     img: "/island.jpg",
     title: "3-Mile Long Island Iced Tea",
-    desc: "Alcoholic,Collins Glass, Tequila, Triple sec, light rum, vodka, lemon, sweet sour gin, cola",
+    desc: "Straight from the bars of Long Island, the Long Island Iced Tea was popularized in the 1970s. When looking at a Long Island Iced Tea recipe, this cocktail can seem daunting. It has four different spirits — vodka, rum, gin, and tequila — that don't seem to go together. But mix those with triple sec, lemon juice, and cola and you've got a cocktail that everyone from coast to coast loves.",
     price: "$12.50",
   },
   {
     img: "/screwed.jpg",
-    title: "Absolutly Screwed Up",
-    desc: "Ginger ale, Orange juice,Absolut Citron, triple sec",
+    title: "Absolutely Screwed Up",
+    desc: "This master piece was created by Donn Beach, a legendary tiki bar owner, and has since become a favorite tiki cocktail. It features three kinds of rum, lime juice, falernum, grenadine, simple syrup, and pineapple juice. But be warned, after too many of these, you could be walking around like a zombie.",
     price: "$9.50",
   },
   {
     img: "/alice.jpg",
     title: "Alice in Wonderland",
-    desc: "Amaretto, Southern comfort,  Grand marnier",
+    desc: "The Jungle Bird cocktail originated at the Kuala Lumpur Hilton Hotel in Malaysia in the 1970s. The cocktail was served in the Aviary Bar where tropical birds were on display, hence its name. A mixture of rum, Campari, pineapple juice, lime juice, and demerara syrup, the Jungle Bird was extremely popular in Malaysia for years, but it took decades before it made a splash in the U.S. Now the drink is a staple at The Joint.",
     price: "$13.50",
   },
  
